15579275f2304db8d626ee3a655e75c03d6ce183
Syafiq <msyafiqpozi@gmail.com>
2020-09-05 23:34:08 +0800

    Configuring postgresql v1

--- app.py
+++ app.py
@@ -96,12 +96,18 @@
 #     app.run(debug=True)
 
 #%% Initial source
-import os
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+from models import Result
+
+
 
 @app.route('/')
 def hello():
