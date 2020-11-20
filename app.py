0ccacfb3b210223be7674b3710a4f2cb30a641bb
sintoka <sintokanalytica@gmail.com>
2020-11-21 00:29:15 +0800

    Investhack Version 1

--- app.py
+++ app.py
@@ -8,13 +8,26 @@ import numpy as np
 from numpy import dot
 from numpy.linalg import norm
 
+#Load the dataset
+stock_df =  pd.read_pickle("./assets/stocks.pkl")
+
+# Put temporary here
+stock_results = {}
+for index, row in stock_df.iterrows():
+    stock_name = row['stock']
+    stock_history = row['history']
+    stock_last_price = stock_history['Close'][-1]
+    stock_results[stock_name] = stock_last_price
 #%% Flask start
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Word2Vec, Candidate, CandidateInfo
+# from models import Word2Vec, Candidate, CandidateInfo
+
+
 
 #%% Natural language processing
 import nltk
@@ -45,9 +58,9 @@ def compute_cossim_percent(x, y):
     cos_sim = 1 - spatial.distance.cosine(arrX, arrY)
     return round(cos_sim*100,2)
 
-def authorIDtoName(x):
-    res = CandidateInfo.query.filter(CandidateInfo.author_id==x).first()
-    return res.author_name
+# def authorIDtoName(x):
+#     res = CandidateInfo.query.filter(CandidateInfo.author_id==x).first()
+#     return res.author_name
 
 
 #%% Flask begin
@@ -55,50 +68,62 @@ def authorIDtoName(x):
 def hello():
     return "Hello World!"
 
-@app.route('/investhack')
+@app.route('/investhack' , methods=['GET', 'POST'])
 def matching():
-    return render_template('investhack.html') 
+    #1. Display stock and its last price
+    stock_results = []
+    for index, row in stock_df.iterrows():
+        stock_name = row['stock']
+        stock_history = row['history']
+        stock_last_price = stock_history['Close'][-1]
+        stock_results.append((stock_name, stock_last_price))
+    #2. Select stock
+    # print(stock_results)
+
+    #3. Output the stock
+    return render_template('investhack.html', result = stock_results) 
 
 
 
-@app.route('/search', methods=['GET', 'POST'])
-def word_vec():
-    if request.method == "POST":
-        query = request.form["query"]
+# @app.route('/search', methods=['GET', 'POST'])
+# def word_vec():
+#     if request.method == "POST":
+#         query = request.form["query"]
 
-        #1. Vectorizing query
-        tokens = textToListProcessing(query)
-        vectors = []
-        for token in tokens:
-            res = Word2Vec.query.filter(Word2Vec.word==token).first()
-            vectors.append(res.vector)
+#         #1. Vectorizing query
+#         tokens = textToListProcessing(query)
+#         vectors = []
+#         for token in tokens:
+#             res = Word2Vec.query.filter(Word2Vec.word==token).first()
+#             vectors.append(res.vector)
 
-        qvector = np.mean(vectors, axis=0)  #Average the vector
-        qlist = qvector.tolist()
+#         qvector = np.mean(vectors, axis=0)  #Average the vector
+#         qlist = qvector.tolist()
 
-        #2. Get a list of authors to measure the similarity (from postgresql)
-        aid_res = Candidate.query.all()
-        author_list = []
-        for aid in aid_res:
-            author_list.append((aid.id, aid.author_id, aid.vector)) 
+#         #2. Get a list of authors to measure the similarity (from postgresql)
+#         aid_res = Candidate.query.all()
+#         author_list = []
+#         for aid in aid_res:
+#             author_list.append((aid.id, aid.author_id, aid.vector)) 
 
-        #Convert list to dataframe
-        df = pd.DataFrame(author_list, columns=['id', 'author_id', 'vector'])
-        df['cos_sim'] = df.drop(columns=['id', 'author_id']).apply(compute_cossim_percent, axis=1, args=[qlist])
-        df2 = df.sort_values(by=['cos_sim'], ascending=False).drop_duplicates('author_id').sort_index()
-        df2.drop(columns=['id', 'vector'], inplace=True)
-        df2 = df2.sort_values(by=['cos_sim'], ascending=False)
+#         #Convert list to dataframe
+#         df = pd.DataFrame(author_list, columns=['id', 'author_id', 'vector'])
+#         df['cos_sim'] = df.drop(columns=['id', 'author_id']).apply(compute_cossim_percent, axis=1, args=[qlist])
+#         df2 = df.sort_values(by=['cos_sim'], ascending=False).drop_duplicates('author_id').sort_index()
+#         df2.drop(columns=['id', 'vector'], inplace=True)
+#         df2 = df2.sort_values(by=['cos_sim'], ascending=False)
 
-        #Replace author_id to name
-        df2['author_name'] = df2['author_id'].apply(authorIDtoName) 
+#         #Replace author_id to name
+#         df2['author_name'] = df2['author_id'].apply(authorIDtoName) 
 
-        #3. Visualize
-        #Transform dataframe to dictionary
-        result = df2.set_index('author_name').to_dict()['cos_sim']
+#         #3. Visualize
+#         #Transform dataframe to dictionary
+#         result = df2.set_index('author_name').to_dict()['cos_sim']
 
-        #Return to html
-        return render_template('index.html', result = result) 
-    return render_template('index.html', result = {})
+#         #Return to html
+#         return render_template('index.html', result = result) 
+#     return render_template('index.html', result = {})
 
 if __name__ == '__main__':
     app.run()
+# %%
